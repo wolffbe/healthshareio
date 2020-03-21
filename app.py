9b2b7dee925de0f4fa6fac08da2867be824188e9
sxphiastx <sophia@stanienda.info>
2020-03-21 18:25:59 +0100

    nicht fertig!!

--- app.py
+++ app.py
@@ -1,25 +1,127 @@
-from flask import Flask, render_template, json, request
-
+from flask import Flask, render_template, json, request, url_for, redirect, flash 
 from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+
+
 
 
 app = Flask(__name__)
+mydb = mysql.connector.connect(
 
+        host="52.166.36.96",
+        user="app",
+        passwd="covid789", 
+        database ="hackathon"
+    )       
 
+print(mydb)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/showSignup')
+@app.route('/Signup', methods=['GET', 'POST'])
 def showSignUp():
-    return render_template('signup.html')
+    #bei Get soll er das nur rendern .. 
+    if request.method=='POST': 
+        
+        details = request.form
+        inputName = details['inputName']
+        inputAddress= details['inputAdress']
+        inputContact = details['inputAnsprechpartner']
+        inputTelephone = details['inputTelephone']
+        inputNeed = details['inputNeed']
+        inputAnzahlNeed = details['inputAnzahlNeed']
+        inputGive= details['inputGive']
+        inputAnzahlGive = details['inputAnzahlGive']
 
-@app.route('/showLogin')
+       
+        if details['inputInstitution'] == "Medizinische Einrichtung": 
+            institutionType = "medical"
+        elif details['inputInstitution'] == "Unternehmen": 
+            institutionType =  "company"
+        else: 
+            institutionType="undefined"
+        
+   
+        
+        sql_institution = "INSERT INTO tbl_institutions (name, type, address, contact, telephone, lat, lng) VALUES(%s,%s,%s,%s,%s,%s.%s)"
+        values_institution = (inputName, institutionType,inputAddress, inputContact, inputTelephone, 0.0, 0.0)
+
+        print(sql_institution)
+        print(values_institution)
+        
+        cur = mydb.cursor()
+        try: 
+            cur.execute(sql_institution, values_institution)
+            mydb.commit()
+            
+            cur.execute( "Select last_insert_id()")
+            inst_id = mydb.commit()
+
+        except: 
+            error = "Something went wrong while inserting the data into tbl_institution"
+            print(error)
+            return render_template('signup.html', error=error)
+        
+        #print(inst_id)
+        return("ok")
+        # objecttypes = [{"Desinfektionsmittel":"disinfectants"}, {"Masken" : "masks"}, {"Handschuhe": "gloves"}]
+
+        # if inputNeed and inputAnzahlNeed: 
+        #     for object in objecttypes: 
+        #         for key in object: 
+        #             if inputNeed == key:
+        #                 objectypeDB = objecttypes['object']['key']
+            
+
+            
+        #     sql_demand= "INSERT INTO tbl_demand(fk_institution, objecttype, amount) VALUES (%s, %s,%s)"
+
+        #     values_demand=(inst_id, objectypeDB, inputAnzahlNeed )
+        #     try: 
+        #         cur.execute(sql_demand, values_demand)
+        #         mydb.commit()
+        #     except: 
+        #         error = "Something went wrong while inserting the data into tbl_demand"
+        #         return render_template('signup.html', error=error)
+        
+        # return redirect("/map")
+
+
+        #setzen der variablen
+        #generate sql und execute - für die institution
+        # generelt dann demand 
+        # fehler abfangen - if fehler dann error zurück 
+        # if no fehler -- redirect--auf map.html 
+
+    
+
+    elif request.method=='GET':
+        return render_template('signup.html')
+
+@app.route('/showLogin',methods=['GET','POST'])
 def showLogin():
-    return render_template('login.html')
+    if request.method=='POST':
+        details=request.form
+        fName=details['fname']
+        lName=details['lname']
+        email='hallo'
+        
+        sql = "INSERT INTO users(username, password, email) VALUES (%s, %s, %s)"
+        val=(fName, lName, email)
+        
+        cur=mydb.cursor()
+        #cur.execute("INSERT INTO users(username, password, email) VALUES (%s, %s,%s)",(fName,lName,email))
+        cur.execute(sql, val)
+        mydb.commit()
+        cur.close()
+    
+        return('success')
+    
+    return render_template('login.html') 
 
-@app.route('/showMap')
+@app.route('/map')
 def showMap():
     return render_template('map.html')
 
@@ -37,3 +139,4 @@ def signUp():
 
 if __name__ == "__main__":
     app.run(port=5000)
+
