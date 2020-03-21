857af76a8673fb8bd2fd650877db3e856cfae5ae
Benedict Wolff <benedictwolff@gmail.com>
2020-03-21 18:29:43 +0100

    added map

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, json, request
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
+import requests
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import requests
 
 mysql = MySQL()
 app = Flask(__name__)
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
-app.config['MYSQL_DATABASE_DB'] = 'BucketList'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+app.config["DEBUG"] = True
+CORS(app)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/showSignup')
+@app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
 
 @app.route('/showLogin')
-def showMap():
+def showLogin():
     return render_template('login.html')
 
 @app.route('/showMap')
@@ -29,44 +28,12 @@ def showMap():
     return render_template('map.html')
 
 @app.route('/showProfile')
-def showMap():
+def showProfile():
     return render_template('profile.html')
 
 @app.route('/showDeliveries')
-def showMap():
+def showDeliveries():
     return render_template('deliveries.html')
-
-@app.route('/signup',methods=['POST','GET'])
-def signUp():
-    try:
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
-
-        # validate the received values
-        if _name and _email and _password:
-            
-            # All Good, let's call MySQL
-            
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-                return json.dumps({'message':'User created successfully !'})
-            else:
-                return json.dumps({'error':str(data[0])})
-        else:
-            return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-    except Exception as e:
-        return json.dumps({'error':str(e)})
-    finally:
-        cursor.close() 
-        conn.close()
 
 if __name__ == "__main__":
     app.run(port=5000)
